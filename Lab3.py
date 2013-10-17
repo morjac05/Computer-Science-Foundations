77d32e27c290e0f78d158d73c1008a4bc44e0d21
morjac05 <jakemorris9875@gmail.com>
2013-10-17 14:59:22 -0700

    corrected quiz 1

---
+++ Lab3.py
@@ -0,0 +1,12 @@
+n=5
+x=1
+y=0
+series='fibonacci'
+
+if(series=='fibonacci'):
+    for i in range(1,n+1)
+        x+y
+        x=x+y
+        x+y
+        y=x+y
+        print x,y
---
+++ quiz1.py
@@ -0,0 +1,5 @@
+# Graded by Cardin Nguyen
+# ngutro25
+
+from lab2 import n
+print "Hello World \n" * n
